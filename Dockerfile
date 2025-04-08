53c12e00ab5a6fea15b2b78158e069ee9605c580
Lachlan Stevens <stevens.lachlan@gmail.com>
2025-04-08 19:45:14 +1000

    Created Dockerfile and entrypoint for Peertube Runner 0.1.0

---
+++ Dockerfile
@@ -0,0 +1,32 @@
+FROM node:23-slim
+
+LABEL org.opencontainers.image.source=https://github.com/fediverse-games/peertube-runner-docker
+LABEL org.opencontainers.image.description="Peertube Runner"
+LABEL org.opencontainers.image.licenses=GPL-3.0-or-later
+
+
+ENV DEBIAN_FRONTEND noninteractive
+ENV FFMPEG_THREADS 0
+ENV FFMPEG_CONCURRENT_JOBS 2
+ENV FFMPEG_NICE 20
+ENV PEERTUBE_CONFIG_DIR = "/home/peertube/.config/peertube-runner-nodejs/default"
+ENV PEERTUBE_CONFIG "config.toml"
+ENV PEERTUBE_RUNNER_NAME ""
+ENV PEERTUBE_URL ""
+ENV PEERTUBE_RUNNER_TOKEN ""
+ENV PEERTUBE_RUNNER_DESCRIPTION "Peertube Runner"
+
+WORKDIR /home/peertube/
+
+ENV HOME=/home/peertube
+
+COPY entrypoint.sh /
+
+RUN apt update \
+    && apt install -y pipx ffmpeg --no-install-recommends \
+    && apt clean && rm -rf /var/lib/apt/lists/* && pipx install whisper-ctranslate2 && pipx ensurepath && npm i @peertube/peertube-runner@0.1.0 && chmod +x /entrypoint.sh
+
+VOLUME [ "/home/peertube/.config/peertube-runner-nodejs/" ]
+
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail            
+             
+echo 'Starting Peertube Runner docker container... container maintained by Fediverse.Games'
+echo 'For any issues or assistance, check us out on GitHub:'
+echo 'https://github.com/fediverse-games/peertube-runner-docker'    
+           
+echo 'Building peertube config...'
+
+if [ -z ${PEERTUBE_RUNNER_NAME}]
+then
+    export PEERTUBE_RUNNER_NAME=${HOSTNAME}
+fi
+
+if [ -f ${PEERTUBE_CONFIG} ]
+then
+    echo "Mounted config file detected, copying to config directory."
+    mv ${PEERTUBE_CONFIG} ${PEERTUBE_CONFIG_DIR}/config.toml
+else
+    echo "Creating config file from env..."
+    if [ -z ${PEERTUBE_URL} ]
+    then
+        echo 'ERROR: Peertube URL required to run container. Terminating...'
+        exit 1
+    fi
+
+    if [ -z ${PEERTUBE_RUNNER_TOKEN} ]
+    then
+        echo 'ERROR: Peertube runner token required to run container. Terminating...'
+        exit 1
+    fi
+    export PEERTUBE_CONFIG_DIR="/home/peertube/.config/peertube-runner-nodejs/${PEERTUBE_RUNNER_NAME}"
+    mkdir $PEERTUBE_CONFIG_DIR
+    cat > "$PEERTUBE_CONFIG_DIR/$PEERTUBE_CONFIG" <<EOF
+[jobs]
+concurrency = ${FFMPEG_CONCURRENT_JOBS}
+
+[ffmpeg]
+threads = ${FFMPEG_THREADS}
+nice = ${FFMPEG_NICE}
+
+[transcription]
+engine = "whisper-ctranslate2"
+model = "small"
+
+[registeredInstances]
+url = "${PEERTUBE_URL}"
+runnerToken = "${PEERTUBE_RUNNER_TOKEN}"
+runnerName = "${PEERTUBE_RUNNER_NAME}"
+runnerDescription = "${PEERTUBE_RUNNER_DESCRIPTION}"    
+
+EOF
+    
+fi
+echo "Starting peertube runner now..."
+trap "echo 'Termination command received. Deregistering runner and terminating...'; npx peertube-runner unregister --id ${PEERTUBE_RUNNER_NAME} --runner-name ${PEERTUBE_RUNNER_NAME} --url ${PEERTUBE_URL}" SIGTERM
+npx peertube-runner server --id ${PEERTUBE_RUNNER_NAME} & sleep 2s; npx peertube-runner register --id ${PEERTUBE_RUNNER_NAME} --runner-name ${PEERTUBE_RUNNER_NAME} --url ${PEERTUBE_URL} --registration-token ${PEERTUBE_RUNNER_TOKEN} & wait
